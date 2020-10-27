4a46160422b3433b906cfc3eb7f7dd8768447143
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-10-27 14:48:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
 START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I'm Torrent Searcher Bot 🤖, you can search Torrents and get the Magnet links of it using me just send me a search query to get started.*")
-keyboard = [[InlineKeyboardButton(text="👨🏻‍💻 DEVELOPER",url="https://t.me/neil_arms")]]
+keyboard = [[InlineKeyboardButton(text= 
 FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Powered By @Neil_Projects*")

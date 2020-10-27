c11f8e69153df809f5b1b64a7da0bd9047d64530
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-10-27 12:50:40 +0530

    Update inline.py

--- inline/inline.py
+++ inline/inline.py
@@ -87,16 +87,16 @@ def button(update, context):
     query = update.callback_query
     query.answer()
     torrent_name = query.data
-    query.edit_message_text(text="*🤓 Fetching details... Please wait!*")
+    query.edit_message_text(text="🤓 Fetching details... Please wait!")
     if torrent_name == None:
-        query.edit_message_text(text="*🙄 Something went wrong*")
+        query.edit_message_text(text="🙄 Something went wrong")
         return
     response = torrent_search(torrent_name)
     if response == None:
-        query.edit_message_text(text="*🙄 Something went wrong*")
+        query.edit_message_text(text="🙄 Something went wrong")
         return
     if len(response) == 0:
-        query.edit_message_text(text="*🙄 Something went wrong*")
+        query.edit_message_text(text="🙄 Something went wrong")
         return
 
     

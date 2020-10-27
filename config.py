913f31a475e5e8084d0feefe1c9d6838d599efd7
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-10-27 14:42:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
-START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I am a simple torrent searcher using @sjprojects's Torrent Searcher api.\n\n\nMade with 🐍 by @KeralasBots*")
+START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I'm Torrent Searcher Bot 🤖, you can search Torrents and get the Magnet links of it using me just send me a search query to get started.*")
 keyboard = [[InlineKeyboardButton(text="👨🏻‍💻 DEVELOPER",url="https://t.me/neil_arms")]]
 FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Powered By @Neil_Projects*")

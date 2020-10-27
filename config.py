8cb089dd7dcfea00cb231cccdc880575714e4df7
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-10-27 14:56:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
 START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I'm Torrent Searcher Bot 🤖, you can search Torrents and get the Magnet links of it using me just send me a search query to get started.*")
-
 FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Powered By @Neil_Projects*")
